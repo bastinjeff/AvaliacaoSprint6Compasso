50dddd0a8dd99809ebaee82e07e81caa3e506338
Jefferson Frigo <jrfrigo1@gmail.com>
2021-10-26 13:02:36 -0300

    Criação SysProduto Models Configuration

---
+++ ConjuntoApiSprint6/ConjuntoApiSprint6/ModelConfiguration/SysProduto/TagConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConjuntoApiSprint6.ModelConfiguration.SysProduto
+{
+	public class TagConfiguration
+	{
+	}
+}

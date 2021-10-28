2461fc5a173144e7732c4ef6465626325a2e77b4
Jefferson Frigo <jrfrigo1@gmail.com>
2021-10-27 22:05:32 -0300

    Criação da classe responsável por operar o MongoDB
    Ainda sem implementação, mas a classe já existe

---
+++ ConjuntoApiSprint6/ConjuntoApiSprint6/Models/Auditoria/ErrorObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConjuntoApiSprint6.Models.Auditoria
+{
+	public class ErrorObject
+	{
+	}
+}
---
+++ ConjuntoApiSprint6/ConjuntoApiSprint6/Operations/Auditoria/MongoDbOperations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConjuntoApiSprint6.Operations.Auditoria
+{
+	public class MongoDbOperations
+	{
+	}
+}
